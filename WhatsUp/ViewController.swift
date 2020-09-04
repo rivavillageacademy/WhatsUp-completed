6963136e2bc07157c40fa63a0282e2ab51d47299
Muhamed Alkhatib <malkhatib@gmail.com>
2020-09-05 00:22:45 +0400

    Initial Commit

---
+++ WhatsUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsUp
+//
+//  Created by Muhamed Alkhatib on 05/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
